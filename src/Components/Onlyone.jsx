d87344c982a389a7ef9b250866c1e6074d85da81
rimelJB <rimel1jabnoun@gmail.com>
2021-09-16 07:36:33 +0100

    second commit

--- src/Components/Onlyone.jsx
+++ src/Components/Onlyone.jsx
@@ -2,24 +2,40 @@ import React from 'react';
 import Logo from './Logo.png';
 import './Onlyone.css';
 import {Row,Col} from 'react-bootstrap';
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Onlyone = () => {
     return (
         <div>
               <Row>
-                <Col><div style={{border:"solid 1px black",maxWidth:"100vw"}}>
-                    <h1 className='title'>Your name here</h1></div>
+              <Col></Col>
+                <Col xs={5}><div style={{border:"solid 1px black",maxWidth:"100vw"}}><h2 className='title'>Your name here</h2>
+                </div>
+                    
                 </Col>
-            
+                <Col></Col>
+                </Row>
                      <br/>
     
-                
-                    <Col xs={20}><img src="/tunisia.png"  alt='myimg'/></Col>
-
+                <Row>
+                    <Col></Col>
+                    <Col xs={5}><img src="/tunisia.png"  alt='myimg'/></Col>
+                    <Col></Col>
+                </Row>
                     <br/>
-
-               
-                    <Col xs={20}><img src={Logo} alt='logo' /></Col>
+                   
+                 <Row>
+                 <Col></Col>
+                    <Col xs={5}><img src={Logo} alt='logo' /></Col>
+                <Col></Col>
+                </Row>
+                    <br/>
+                    <Row>
+                 <Col></Col>
+                 <Col lg={5}>
+                <video width="320" height="240" controls>
+                <source src="https://www.youtube.com/watch?v=_IVL0Sktcx0" type="video/mp4"/></video></Col>
+                <Col></Col>
                 </Row>
                     <br/>
             
